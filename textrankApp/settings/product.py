97c4970d0c1515870a755439726ac42bbebfa8ab
정영희 <69373688+jkhyjkhy@users.noreply.github.com>
2023-12-07 17:49:43 +0900

    Update product.py

--- textrankApp/settings/product.py
+++ textrankApp/settings/product.py
@@ -8,18 +8,11 @@ SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = False
 
 # AWS Example
-ALLOWED_HOSTS = ['.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['jkhyjkhy.pythonanywhere.com']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ['DATABASE_NAME'],
-        'USER': os.environ['DATABASE_USER'],
-        'PASSWORD': os.environ['DATABASE_PASSWORD'],
-        'HOST': 'django-mysql.ap-northeast-2.rds.amazonaws.com',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
