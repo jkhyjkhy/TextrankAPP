577209a788357ad76b6a0d1a54e0fc720d9020a7
정영희 <69373688+jkhyjkhy@users.noreply.github.com>
2023-12-07 17:37:37 +0900

    Update develop.py

--- textrankApp/settings/develop.py
+++ textrankApp/settings/develop.py
@@ -1,16 +1,1 @@
-from .base import *
 
-SECRET_KEY = "django-insecure-dz*khep9b_5@d7+qmwjb^&u*w86c&gs&_piy7$om!6t3_qrl)3"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
