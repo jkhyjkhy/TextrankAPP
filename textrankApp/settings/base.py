1c6c62a3c6aec7898dba206fa7ffc5d9ed375889
정영희 <69373688+jkhyjkhy@users.noreply.github.com>
2023-12-07 17:33:35 +0900

    delete KEY

--- textrankApp/settings/base.py
+++ textrankApp/settings/base.py
@@ -19,9 +19,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "django-insecure-dz*khep9b_5@d7+qmwjb^&u*w86c&gs&_piy7$om!6t3_qrl)3"
-#
+
 # # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 #
